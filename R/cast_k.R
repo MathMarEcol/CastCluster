697e02544f4353c4622c93924ce80eb4227cc5eb
Phil Dyer <phildyer@protonmail.com>
2020-05-21 16:44:58 +1000

    Clean and condense logic on cast_k
    Now makes use of existing functions
    to quickly find good solutions

--- R/cast_k.R
+++ R/cast_k.R
@@ -20,101 +20,42 @@ cast_k <- function(sim_mat, k,
   assertthat::assert_that(k > 1)
   assertthat::assert_that(k <= nrow(sim_mat))
 
-  n <- nrow(sim_mat)
-  spares <- seq.int(n)
-
-  seeds <- sample.int(n, k, replace = FALSE)
-  cast_ob <- lapply(seeds, function(x){c(x)})
-  spares <- spares[-seeds]
-
-  ##matrix, mean affinity of each site to each cluster
-  ##two cols must be updated each iteration
-  ## clust_aff <- aff_clust_all(sim_mat, cast_test)
-  clust_aff_sum_master <- aff_clust_sum(sim_mat, cast_ob)
-  ## matrix, sum affinity and cluster N matrix
-  clust_aff_n_master <- do.call(c, lapply(cast_ob, length))
-  clust_aff_master <- clust_aff_sum_master * rep(1 / clust_aff_n_master, each = nrow(clust_aff_sum_master))
-  clust_aff_sum <- clust_aff_sum_master
-  clust_aff_n <- clust_aff_n_master
-  clust_aff <- clust_aff_master
-  ##Assign all spares, iteratively, greedy
-  while(length(spares) > 0) {
-
-    clust_aff_spares <- cbind(seq.int(n), clust_aff)[spares, , drop = FALSE]
-
-    max_ind <- which.max(clust_aff_spares[,-1])
-    max_ind_zero <- (max_ind - 1)
-    i <- 1 + (max_ind_zero %% nrow(clust_aff_spares)) #modulo is zero based indexing, R is 1 based indexing
-    to <- (max_ind - i)/nrow(clust_aff_spares) + 1
-    upd <- data.frame(spare = clust_aff_spares[i,1], to = to)
-
-    to_clust <- c(cast_ob[[upd$to]], upd$spare)
-    spares <- spares[-i]
-
-    clust_sum_to <- clust_aff_sum[, upd$to] + sim_mat[, upd$spare]
-    clust_aff_to <- (1/(clust_aff_n[upd$to] + 1)) *  mean(clust_sum_to[to_clust])
-
-    cast_ob[[upd$to]] <- to_clust
-
-    clust_aff_n[upd$to] <- clust_aff_n[upd$to] + 1
-    clust_aff_sum[, upd$to] <- clust_sum_to
-    clust_aff[, upd$to] <- clust_aff_sum[, upd$to] * (1 / clust_aff_n[upd$to])
-  }
+  aff_thres <- mean(sim_mat) ##need a stopping point. Subdivide if too broad
 
 
+  ##get an initial CAST clustering and stabilize
+  cast_ob <- cast_alg(sim_mat, aff_thres)
+  cast_ob <- cast_stabilize(cast_ob, aff_thres = 0, sim_mat)
 
-  ##Stabilize the clustering
-  clust_ind <- lapply(seq_along(cast_ob), function(clust, cast_ob){
-    data.frame(elem = cast_ob[[clust]], clust = clust)
-  }, cast_ob = cast_ob)
-  clust_ind <- do.call(rbind, clust_ind)
-  clust_ind <- clust_ind[order(clust_ind$elem), ]
+  ##Add or remove clusters until k is reached.
+  while(length(cast_ob) != k){
+    clust_sim <- aff_cluster_between(cast_ob, sim_mat)
+    clust_sim_mat <- matrix(clust_sim$affs, nrow = length(cast_ob), ncol =length(cast_ob) )
+    if(length(cast_ob) > k){
+      ##Merge two most similar clusters
+      clust_sim <- clust_sim[!(clust_sim$Var1 == clust_sim$Var2),]
+      clust_merge <- clust_sim[which.max(clust_sim$affs),]
+      cast_ob[[clust_merge$Var1]] <- c(cast_ob[[clust_merge$Var1]], cast_ob[[clust_merge$Var2]])
+      cast_ob[[clust_merge$Var2]] <- NULL
 
-  iter <- 1
-  while(iter <= max_iter){
-    ind <- clust_ind$elem + (clust_ind$clust - 1) * nrow(clust_ind)
-    clust_current <- clust_aff[ind]
-    gain <- clust_aff - clust_current
-
-    max_ind <- which.max(gain)
-    max_ind_zero <- (max_ind -1)
-    i <- 1 + (max_ind_zero %% nrow(clust_ind)) #modulo is zero based indexing, R is 1 based indexing
-    from <- clust_ind$clust[i]
-    to <- (max_ind - i)/nrow(clust_ind) + 1
-    upd <- data.frame(i = i, from = from, to = to, gain = gain[max_ind])
-
-    if(upd$gain <= 0){
-      ##All moves would take an object out of the best cluster.
-      break
+      ##Stabilise
+      cast_ob <- cast_stabilize(cast_ob, aff_thres = 0, sim_mat)
     } else {
+      ##Seed the weakest object in the weakest cluster into a new cluster
+      clust_sim <- clust_sim[(clust_sim$Var1 == clust_sim$Var2),]
+      clust_split <- clust_sim[which.min(clust_sim$affs),1]
+      affs <- aff_clust_all(sim_mat, cast_ob)
+      new_seed <- which(affs[ , clust_split] ==  min(affs[cast_ob[[clust_split]],clust_split]))
+      cast_ob[[clust_split]] <- cast_ob[[clust_split]][cast_ob[[clust_split]] != new_seed]
+      cast_ob[[length(cast_ob) + 1]] <- c(new_seed)
 
-      to_clust <- c(cast_ob[[upd$to]], upd$i)
-      from_clust <- cast_ob[[ upd$from ]][cast_ob[[ upd$from ]] != upd$i  ] 
-      clust_sum_to <- clust_aff_sum[, upd$to] + sim_mat[, upd$i]
-      clust_aff_to <- 1/(clust_aff_n[upd$to] + 1) *  mean(clust_sum_to[to_clust])
-      clust_sum_from <- clust_aff_sum[, upd$from] - sim_mat[, upd$i]
-      clust_aff_from <- 1/(clust_aff_n[upd$from] - 1) * mean(clust_sum_from[from_clust])
-
-      cast_ob[[upd$to]] <- to_clust
-      cast_ob[[upd$from]] <- from_clust
-
-      clust_ind[upd$i, 2] <- upd$to
-
-      clust_aff_n[upd$to] <- clust_aff_n[upd$to] + 1
-      clust_aff_sum[, upd$to] <- clust_sum_to
-      clust_aff[, upd$to] <- clust_aff_sum[, upd$to] * (1 / clust_aff_n[upd$to])
-
-      clust_aff_n[upd$from] <- clust_aff_n[upd$from] - 1
-      clust_aff_sum[, upd$from] <- clust_sum_from
-      clust_aff[, upd$from] <- clust_aff_sum[, upd$from] * (1 / clust_aff_n[upd$from])
-
-      all_clust_affs[upd$from] <- clust_aff_from
-      all_clust_affs[upd$to] <- clust_aff_to
+      ##Stabilise
+      cast_ob <- cast_stabilize(cast_ob, aff_thres = 0, sim_mat)
 
     }
 
   }
 
-
+  return(cast_ob)
 
 }
