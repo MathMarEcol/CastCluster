1a28423e59674a6e70f38487b1a2de3f19c0aad9
Phil Dyer <phildyer@protonmail.com>
2020-05-26 14:10:04 +1000

    Fix logic, add by split global weakest, merge by removing

--- R/cast_k.R
+++ R/cast_k.R
@@ -13,6 +13,7 @@
 #' @param k number of clusters to fit
 #' @param max_iter don't shuffle objects forever
 cast_k <- function(sim_mat, k,
+                   aff_thres = mean(sim_mat),
                    max_iter = nrow(sim_mat)*2){
 
   assertthat::assert_that(isSymmetric(unname(sim_mat)))
@@ -20,7 +21,6 @@ cast_k <- function(sim_mat, k,
   assertthat::assert_that(k > 1)
   assertthat::assert_that(k <= nrow(sim_mat))
 
-  aff_thres <- mean(sim_mat) ##need a stopping point. Subdivide if too broad
 
 
   ##get an initial CAST clustering and stabilize
@@ -31,6 +31,22 @@ cast_k <- function(sim_mat, k,
   while(length(cast_ob) != k){
     clust_sim <- aff_cluster_between(cast_ob, sim_mat)
     clust_sim_mat <- matrix(clust_sim$affs, nrow = length(cast_ob), ncol =length(cast_ob) )
+    ##get mapping from elements to clusters
+    clust_ind <- lapply(seq_along(cast_ob), function(clust, cast_ob){
+      if(length(cast_ob[[clust]]) > 0) {
+        return(data.frame(elem = cast_ob[[clust]], clust = clust))
+      } else {
+        return(NULL)
+      }
+    }, cast_ob = cast_ob)
+    clust_ind <- do.call(rbind, clust_ind)
+    clust_ind <- clust_ind[order(clust_ind$elem), ]
+    ind <- clust_ind$elem + (clust_ind$clust - 1) * nrow(clust_ind)
+
+    ## matrix, sum affinity and cluster N matrix
+    clust_aff_sum <- aff_clust_sum(sim_mat, cast_ob)
+    clust_aff_n <- do.call(c, lapply(cast_ob, length))
+    clust_aff <- clust_aff_sum * rep(1 / clust_aff_n, each = nrow(clust_aff_sum))
     if(length(cast_ob) > k){
       ## ##Merge two most similar clusters
       ## clust_sim <- clust_sim[!(clust_sim$Var1 == clust_sim$Var2),]
@@ -45,24 +61,10 @@ cast_k <- function(sim_mat, k,
       ##Simplify, I just pick one move at a time until all sites are gone
       ##then use cast_stabilize() to finish then job
 
-      ## matrix, sum affinity and cluster N matrix
-      clust_aff_sum <- aff_clust_sum(sim_mat, cast_ob)
-      clust_aff_n <- do.call(c, lapply(cast_ob, length))
-      clust_aff <- clust_aff_sum * rep(1 / clust_aff_n, each = nrow(clust_aff_sum))
       clust_aff[, clust_remove] <- -2
 
-      ##get mapping from elements to clusters
-      clust_ind <- lapply(seq_along(cast_test), function(clust, cast_test){
-        if(length(cast_test[[clust]]) > 0) {
-          return(data.frame(elem = cast_test[[clust]], clust = clust))
-        } else {
-          return(NULL)
-        }
-      }, cast_test = cast_test)
-      clust_ind <- do.call(rbind, clust_ind)
-      clust_ind <- clust_ind[order(clust_ind$elem), ]
-      while(length(cast_ob[[clust_remove]]) != 0 ){
-        ind <- clust_ind$elem + (clust_ind$clust - 1) * nrow(clust_ind)
+      iter <- 0
+      while((length(cast_ob[[clust_remove]]) != 0) && (iter < max_iter) ){
         clust_current <- clust_aff[ind]
         gain <- clust_aff - clust_current
         max_ind <- which.max(gain)
@@ -78,25 +80,29 @@ cast_k <- function(sim_mat, k,
         cast_ob[[upd$from]] <- cast_ob[[ upd$from ]][cast_ob[[ upd$from ]] != upd$i  ] 
 
         clust_ind[upd$i, 2] <- upd$to
+        ind[upd$i] <- max_ind
 
         clust_aff_n[upd$to] <- clust_aff_n[upd$to] + 1
         clust_aff_sum[, upd$to] <- clust_aff_sum[, upd$to] + sim_mat[, upd$i]
         clust_aff[, upd$to] <- clust_aff_sum[, upd$to] * (1 / clust_aff_n[upd$to])
 
+        iter <- iter + 1
       }
+
+      assertthat::assert_that(length(cast_ob[[clust_remove]]) == 0)
+      cast_ob[[clust_remove]] <- NULL
 
 
       ##Stabilise
       cast_ob <- cast_stabilize(cast_ob, aff_thres = 0, sim_mat)
     } else {
       ##Seed the weakest object into a new cluster
-      affs <- castcluster::aff_clust_all(sim_mat, cast_ob)
-      new_seed <- which.min(affs)
-      new_seed_zero <- (new_seed - 1)
-      i <- 1 + (new_seed_zero %% nrow(affs)) #modulo is zero based indexing, R is 1 based indexing
-      from <- (max_ind - i)/nrow(affs) + 1
-      cast_ob[[from]] <- cast_ob[[from]][cast_ob[[from]] != i]
-      cast_ob[[length(cast_ob) + 1]] <- c(i)
+      clust_current <- clust_aff[ind]
+
+      new_seed <- which.min(clust_current)
+      from <- clust_ind$clust[new_seed]
+      cast_ob[[from]] <- cast_ob[[from]][cast_ob[[from]] != new_seed]
+      cast_ob[[length(cast_ob) + 1]] <- c(new_seed)
 
       ##Stabilise
       cast_ob <- cast_stabilize(cast_ob, aff_thres = 0, sim_mat)
